bb2ac72d685e836dc3a54a73809affb4823a9589
michal <knapikgm@gmail.com>
2021-06-14 16:43:54 +0200

    wyn

--- src/main/java/gsprod/RandomNetGenerator.java
+++ src/main/java/gsprod/RandomNetGenerator.java
@@ -1,39 +1,34 @@
 package gsprod;
 
-import java.util.ArrayList;
+import java.util.LinkedHashSet;
+import java.util.Random;
 
 public class RandomNetGenerator {
 
-    private int minbf;
-    private int maxbf;
-    private int depth;
-    private int minautsize;
-    private int maxautsize;
+   public static void DFSNetGen(String prefix, int minBranchingFactor, int maxBranchingFactor, int depth,
+                         RandomAutomatonGenerator gen, AutomataNet net, Random rng) {
 
-    public RandomNetGenerator(int minbf, int maxbf, int depth, int minautsize, int maxautsize) {
-        this.minbf = minbf;
-        this.maxbf = maxbf;
-        this.depth = depth;
-        this.minautsize = minautsize;
-        this.maxautsize = maxautsize;
-    }
+                         int branchingFactor = rng.nextInt(maxBranchingFactor - minBranchingFactor + 1) + minBranchingFactor;
+                         LinkedHashSet<String> syncActs = new LinkedHashSet<>();
+                         LinkedHashSet<String> syncDownActs = new LinkedHashSet<>();
+                         for (int i = 0; i < branchingFactor; ++i) syncDownActs.add(prefix+i);
 
-  /*  public AutomataNet generate() {
+                         syncActs.add(prefix);
+                         if (depth > 0) syncActs.addAll(syncDownActs);
+                         Automaton node = gen.generate(syncActs);
+                         net.getAutomata().add(node);
+                         net.getActions().addAll(syncDownActs);
+
+                         if (depth > 0)
+                         for (String deeperPrefix: syncDownActs) DFSNetGen(deeperPrefix, minBranchingFactor, maxBranchingFactor, depth-1, gen, net, rng);
+                         }
+
+    public static AutomataNet DFSNetGen(int minBranchingFactor, int maxBranchingFactor, int depth, int minAutoSize, int maxAutoSize) {
+        RandomAutomatonGenerator gen = new RandomAutomatonGenerator(minAutoSize, maxAutoSize);
+        Random rng = new Random();
         AutomataNet net = new AutomataNet();
+        DFSNetGen("", minBranchingFactor, maxBranchingFactor, depth, gen, net, rng);
 
-        // at this stage the synchronisation structure is limited: each child synchronizes over a single action with
-        // its root
-
-//
-//
-//        ArrayList<Automaton>
-
-        new RandomAutomatonGenerator(this.minautsize, this.maxautsize);
-
-
-        // dla kazdego
-
+        return net;
     }
-
-*/
 }
